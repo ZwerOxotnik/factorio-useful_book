f441e94b27d154bf4818bbfddb8282a9b82fd2a7
ZwerOxotnik <zweroxotnik@gmail.com>
2021-12-22 23:06:45 +0300

    Update to 0.9.2

--- control.lua
+++ control.lua
@@ -1,5 +1,3 @@
-local event_handler = require("event_handler")
-
 ---@type table<string, module>
 local modules = {}
 modules["useful_book"] = require("models/useful_book")
@@ -14,4 +12,10 @@ if remote.interfaces["disable-useful_book"] then
 	module.add_commands = nil
 end
 
+local event_handler
+if script.active_mods["zk-lib"] then
+	event_handler = require("__zk-lib__/static-libs/lualibs/event_handler_vZO.lua")
+else
+	event_handler = require("event_handler")
+end
 event_handler.add_libraries(modules)
